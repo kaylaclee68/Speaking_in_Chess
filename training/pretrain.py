aaaf67780a91e931767c20ce4ef3a8f1db78ed52
efrick2002 <evanfrick@berkeley.edu>
2023-11-09 04:19:05 +0000

    added code

--- training/pretrain.py
+++ training/pretrain.py
@@ -1,2 +1,19 @@
 import transformers
+import argparse
 from datasets import load_dataset
+
+def main(args):
+    dataset = load_dataset(args.dataset, streaming=args.stream)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    parser.add_argument("--tokenizer", type=str, required=True)
+    parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--stream", action=argparse.BooleanOptionalAction, default=False)
+
+    args = parser.parse_args()
+
+    main(args)
+
